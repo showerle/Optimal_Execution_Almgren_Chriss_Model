068a59ca576131fa6c3f2a126dfd3938b2d9ef90
showerli <101498661+showerle@users.noreply.github.com>
2023-10-08 00:18:11 +0800

    Update download.py

--- download.py
+++ download.py
@@ -8,6 +8,6 @@ datasets.download(
     from_date="2023-05-01",
     to_date="2023-06-01",
     symbols=["BTCUSDT", "ETHUSDT"],
-    api_key="TD.ch1avFy1nY2ZWp2K.UuERYMSLcaQRL-l.0OWfFqzNcO4cSe6.wtEg0Hka6TybKqi.CntpAvoUKjH0Eyj.BZ-B",
+    api_key="",
 )
 
